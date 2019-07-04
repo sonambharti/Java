dbfc04cfde110a11e65ef1d7ad74211e3c165f38
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2019-07-05 00:03:58 +0530

    Myself

---
+++ MySelf.java
@@ -0,0 +1,9 @@
+public class MySelf
+{
+    public static void main(String[] arg)
+    {
+        System.out.printf("Hello, I am Sonam Bharti.\n");
+        System.out.println("I am pursuing my B.Tech from Nalanda College of Engineering,Chandi.");
+        System.out.print("I am here to learn JAVA.");
+    }
+}
