64ca64b57fa61b794471118abb6c7b1c7d4d8a2a
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2019-07-17 22:17:12 +0530

    calculate Tax

---
+++ Tax.java
@@ -0,0 +1,10 @@
+//Write a program to enter salary and find 10% tax.
+public class Tax
+{
+    public static void main(String[] arg)
+    {
+        int s=50000;
+        int tax = (s*10)/100;
+        System.out.println(tax);
+    }
+}
