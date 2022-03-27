7d7cd8f681bcd02ed6035fd8b2644e2b3e9b701f
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 21:45:39 +0530

    Print Self Introduction

---
+++ Basic Java code/MySelf.java
@@ -0,0 +1,10 @@
+/* Write a program to introduce myself.*/
+public class MySelf
+{
+    public static void main(String[] arg)
+    {
+        System.out.printf("Hello, I am Sonam Bharti.\n");
+        System.out.println("I am pursuing my B.Tech from Nalanda College of Engineering,Chandi.");
+        System.out.print("I am here to learn JAVA.");
+    }
+}
