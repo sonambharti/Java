0301a6f967918ac76d33497c38d90013e8906b24
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 21:51:34 +0530

    Take Input from Command Line

---
+++ Basic Java code/CmdInput.java
@@ -0,0 +1,7 @@
+class CmdInput{
+    public static void main(String args[]){
+        System.out.println("Your first argument is: "+args[0]);
+        System.out.println("Your second argument is: "+args[1]);
+        System.out.println("Your first argument is: "+args[0]+" and second argument is: "+args[1]);
+    }
+}
