061428fc79f56b843540471a15906e8eea788fb8
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-07-27 02:22:43 +0530

    Switch Case example code

--- Basic Java code/UserActivitySelector.java
+++ Basic Java code/UserActivitySelector.java
@@ -8,24 +8,24 @@ public class UserActivitySelector {
 		String userInput = sc.nextLine();
 		
 		switch(userInput){
-		    case "login":
-		        System.out.println("Please, enter your username.");
-		        break;
-	        case "sign_up":
-    	        System.out.println("Welcome!");
-    	        break;
-    	    case "terminate_program":
-		        System.out.println("Thank you, Good Bye!");
-		        break;
-		    case "main_menu":
-		        System.out.println("Main Menu");
-		        break;
-		    case "about_app":
-		        System.out.println("This app is created by Sonam Bharti.");
-		        break;
-		    default:
-		        System.out.println("Please, enter one of the option: login, sign_up, terminate_program, main_menu, about_app");
-		        break;
+			case "login":
+				System.out.println("Please, enter your username.");
+				break;
+			case "sign_up":
+				System.out.println("Welcome!");
+				break;
+			case "terminate_program":
+				System.out.println("Thank you, Good Bye!");
+				break;
+			case "main_menu":
+				System.out.println("Main Menu");
+				break;
+			case "about_app":
+				System.out.println("This app is created by Sonam Bharti.");
+				break;
+			default:
+				System.out.println("Please, enter one of the option: login, sign_up, terminate_program, main_menu, about_app");
+				break;
 		}
 	}
 
